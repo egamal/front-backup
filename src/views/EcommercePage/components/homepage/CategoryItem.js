22582b52215f14216827e58413e605d79cf3557f
EGamal <egamalm@gmail.com>
2021-08-13 17:45:36 -0300

    Agregando categoria todos los productos y filtro por categoria a la home

--- src/views/EcommercePage/components/homepage/CategoryItem.js
+++ src/views/EcommercePage/components/homepage/CategoryItem.js
@@ -9,12 +9,12 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles(styles);
 
-export default function CategoryItem({name, imageUri}) {
+export default function CategoryItem({name, imageUri, code}) {
   const classes = useStyles();
   return (
       <GridItem xs={12} sm={6} md={3}>
         <h4 className={classes.cardTitle}>{name}</h4>
-        <Link to={"#"}>
+        <Link to={`/product-list/${code}`}>
             <Card name={name} className={classes.marginTopZero + " " + classes.boxHover}>
                 <img src={imageUri} alt={name} />
             </Card>
--- src/views/EcommercePage/components/homepage/SectionCategories.js
+++ src/views/EcommercePage/components/homepage/SectionCategories.js
@@ -3,8 +3,12 @@ import React from 'react';
 import styles from '../../../../assets/jss/material-kit-pro-react/views/ecommerceSections/categoriesStyle';
 import { makeStyles } from '@material-ui/core/styles';
 import GridContainer from 'components/Grid/GridContainer';
+import { Link } from 'react-router-dom';
+import Card from 'components/Card/Card';
+import GridItem from 'components/Grid/GridItem';
 import CategoryItem from './CategoryItem';
 import getGridContent from 'views/EcommercePage/helpers/getGridContent';
+import productsImage from 'assets/img/ecommerce/todos_los_productos.jpg';
 
 const useStyles = makeStyles(styles);
 
@@ -19,6 +23,14 @@ export default function SectionCategories({ categories = [] }) {
         <h2>Categorias</h2>
         <GridContainer className={classes.gridCenter}>
           {gridContent}
+          <GridItem xs={12} sm={6} md={3}>
+            <h4 className={classes.cardTitle}>Todos los Productos</h4>
+            <Link to={"/product-list"}>
+                <Card name="Todos los Productos" className={classes.marginTopZero + " " + classes.boxHover}>
+                    <img src={productsImage} alt="todos-los-productos" />
+                </Card>
+            </Link>
+          </GridItem>
         </GridContainer>
       </div>
     </div>
